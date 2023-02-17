fed3452f3ed34129795a9cc0bdaee7f1dcc97b88
alvinjamal <alvinjamalazkya@gmail.com>
2023-02-17 19:41:32 +0700

    Finish

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 const xss = require("xss-clean");
+const helmet = require("helmet");
 const { response } = require("./src/middlewares/common");
 const app = express();
 const upload = require("./src/middlewares/upload");
@@ -11,7 +12,19 @@ const upload = require("./src/middlewares/upload");
 const mainRouter = require("./src/routes/index");
 
 app.use(morgan("dev"));
-app.use(cors("*"));
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: false,
+  })
+);
+// app.use(cors("*"));
+const corsOptions = {
+  origin: "https://storeid-alvin.netlify.app",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
